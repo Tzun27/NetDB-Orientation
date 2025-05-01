1346a151c1fede3d37363155348745ed435a13c2
Tzun27 <chaotzun@gmail.com>
2025-05-01 21:20:03 +0800

    dockerized everything

--- Exercise_3/react_todo/vite.config.ts
+++ Exercise_3/react_todo/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Needed for Docker
+    port: 5173, // Default Vite port
+    strictPort: true,
+    watch: {
+      usePolling: true
+    }
+  }
 })
